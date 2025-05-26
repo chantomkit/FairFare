c74b47f8ac52671f86597b1453a4a94f373566d4
msctomchan <msctomchan@gmail.com>
2025-05-26 22:09:49 +0000

    add conda

--- setup.sh
+++ setup.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+echo "Setting up Conda env for FairFare..."
 
-echo "Setting up FairFare project with Poetry..."
+conda env create -f environment.yml
+conda activate fairfare
 
-# Step 1: Install dependencies using Poetry
-# This command reads the pyproject.toml file and installs all the dependencies
-# including the main and development dependencies.
 echo "Installing dependencies with Poetry..."
 poetry install
 
-# Step 2: Install pre-commit hooks
-# This command installs the pre-commit hooks defined in the .pre-commit-config.yaml file
-# into the Git repository's .git/hooks directory. This ensures that the hooks
-# run automatically before each commit.
 echo "Installing pre-commit hooks..."
 pre-commit install
 
